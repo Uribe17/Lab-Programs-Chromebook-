4c90cb372f9e5d5bcde3128994456dd550ece4d5
Uribe17 <uribei5170@gmail.com>
2024-01-25 19:16:06 -0800

    Create Lab_1.cpp
    This is a program that will output "Hello World" twice.

---
+++ Lab_1.cpp
@@ -0,0 +1,13 @@
+/***
+ * Lab 1
+***/ 
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World Again!\n");
+    return 0;
+}
