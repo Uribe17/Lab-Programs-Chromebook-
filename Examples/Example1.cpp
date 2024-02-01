f28abbab2e2186c1685a0f0b010a9a04d90e0bbe
Uribe17 <157755307+Uribe17@users.noreply.github.com>
2024-02-01 18:51:20 +0000

    Added more to the example code

--- Examples/Example1.cpp
+++ Examples/Example1.cpp
@@ -4,8 +4,12 @@ using namespace std;
 int main()
 {
   int n=23;
-  if (n % 2 == 0) {
+  if (n % 3 == 0) {
     cout << "n is even\n";
+  } else if (n % 2 == 0) {
+    cout << "n is divisible by 2\n";
+  } else if (n % 1 == 0) {
+    cout << "n is divisible by 1\n";
   } else {
       cout<<"n is odd\n"; 
   }
