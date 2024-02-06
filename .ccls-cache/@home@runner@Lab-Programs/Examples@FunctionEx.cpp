edce5420bf2836bc08943acd4a62ad4134c76cf3
Uribe17 <157755307+Uribe17@users.noreply.github.com>
2024-02-06 19:48:46 +0000

    Added MathEx.cpp, updated FunctionEx.cpp

--- .ccls-cache/@home@runner@Lab-Programs/Examples@FunctionEx.cpp
+++ .ccls-cache/@home@runner@Lab-Programs/Examples@FunctionEx.cpp
@@ -6,12 +6,53 @@
  * tan(x)
 ***/
 
-//A function will usually look like:
-//Datatype_return functionName(inputs){}
+/** Function Outputs will usually look like:
+  * Datatype_return functionName(inputs){}
+  * Datatype_return functionName(inputs/arguments/parameters/expressions){}
+  * string getName(string name){
+    return name;
+  }
+  * int getAge(int age){
+    return age;
+  }
+  * bool getsEnrolled(bool isEnroled){
+    return isEnroled;
+  }
+  * Void Function:
+  * void printName(string name)
+***/
+
+/*** How to call on functions:
+  *int main(){
+    string salut = "Hello";
+    string name = "James";
+    datetime today = Datetime().now();
+
+    string output = greet(salut, name, today);
+    cout << output;
+    return 0;
+  }
+***/
 
 // Greet Example
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
 
 string greet(string salutations, string name, datetime date){
   string out = salutations + name + "Today's date is" + date:
   return out;
+}
+
+int main(){
+  string salutations = "Hello";
+  string name = "James";
+  datetime today = datetime().now();
+
+  string output = greet(salutations, name, today);
+  cout << output;
+  return 0;
 }
---
+++ .ccls-cache/@home@runner@Lab-Programs/Examples@MathEx.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+  float output = 0.0;
+  output = sqrt(4.0);
+  cout << output << endl;
+
+  double abs_out = 0.0;
+  abs_out = abs(-2.2);
+  cout << abs_out << endl;
+  return 0;
+}
