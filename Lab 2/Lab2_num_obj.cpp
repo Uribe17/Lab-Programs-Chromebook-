2e02f07fec74b9c46920eb861c9f39016c4d4f6b
Uribe17 <157755307+Uribe17@users.noreply.github.com>
2024-02-06 20:04:36 +0000

    Finished coding both Lab 2 files

--- Lab 2/Lab2_num_obj.cpp
+++ Lab 2/Lab2_num_obj.cpp
@@ -1,6 +1,6 @@
 //Lab2_num_obj.cpp
 /***
-  *
+  * This program will display the size of the multiple examples given
 ***/
 
 #include <iostream>
@@ -14,8 +14,15 @@ int main() {
   double double_example = 2.0;
 
   //non-numerical
-  char
-  bool
+  char charExample = 'a';
+  bool boolExample = true;
 
   //sizeof returns the number of bytes (8 bits = 1 byte)
+  cout << "int_example:" << sizeof(int_example) << endl;
+  cout << "float_example:" << sizeof(float_example) << endl;
+  cout << "double_example:" << sizeof(double_example) << endl;
+
+  cout << "charExample:" << sizeof(charExample) << endl;
+  cout << "boolExample:" << sizeof(boolExample) << endl;
+    
 }
