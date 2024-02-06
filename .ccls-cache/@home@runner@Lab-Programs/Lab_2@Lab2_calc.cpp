21ee799ec230c93a2a2db2f5c3c198d96032d732
Uribe17 <157755307+Uribe17@users.noreply.github.com>
2024-02-06 21:04:09 +0000

    slight changes

---
+++ .ccls-cache/@home@runner@Lab-Programs/Lab_2@Lab2_calc.cpp
@@ -0,0 +1,18 @@
+//Lab2_calc.cpp
+/***
+  * This program will display an int divided by int, and i
+***/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+  //numerical
+  int int_x = 1;
+  int int_y = 2;
+  double double_z = 2.0;
+
+  cout << "int divided by int: " << int_x / int_y << endl;
+  cout << "int divided by double: " << int_x / double_z << endl;
+}
