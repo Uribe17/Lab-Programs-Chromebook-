b36704e761cfbddc9b2c3a71ed26c200d8a9bcf1
Uribe17 <157755307+Uribe17@users.noreply.github.com>
2024-02-06 19:12:25 +0000

    Added a new Function Example, and For loop Example

---
+++ .ccls-cache/@home@runner@Lab-Programs/Examples@FunctionEx.cpp
@@ -0,0 +1,17 @@
+/***
+ * sqrt(x)
+ * pow(x, y)
+ * sin(x)
+ * cos(x)
+ * tan(x)
+***/
+
+//A function will usually look like:
+//Datatype_return functionName(inputs){}
+
+// Greet Example
+
+string greet(string salutations, string name, datetime date){
+  string out = salutations + name + "Today's date is" + date:
+  return out;
+}
