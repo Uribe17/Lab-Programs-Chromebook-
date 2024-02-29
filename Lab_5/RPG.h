1da92d52fc76ef3da33c5b73370b76b774a6b8d7
Uribe17 <uribei5170@gmail.com>
2024-02-29 14:37:43 -0800

    added lab 5

---
+++ Lab_5/RPG.h
@@ -0,0 +1,33 @@
+#ifndef PRG_H
+#define RPG_H
+using namespace std;
+
+const int SKILL_SIZE = 2;
+
+class RPG{
+    public:
+        //Constructors
+        RPG();
+        RPG(string name, int health, int strength, int defense, string type);
+
+        void setSkills();
+        void printAction(string RPG);
+        void updateHealth(int);
+        //These use pointers
+        void attack(RPG *);
+        void useSkill(RPG *);
+
+        bool isAlive() const;
+        string getName() const;
+        int getHealth() const;
+        int getStrength() const;
+        int getDefense() const;
+    private:
+        string name;
+        int health;
+        int strength;
+        int defense;
+        string type;
+        string skills[SKILL_SIZE];
+};
+#endif
