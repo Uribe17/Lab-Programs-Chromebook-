d68a74986f98fd2a995fecc651cbe0d9968d0e9d
Uribe17 <157755307+Uribe17@users.noreply.github.com>
2024-03-07 21:39:18 +0000

    update

--- .ccls-cache/@home@runner@Lab-Programs/Lab_5@main.cpp
+++ .ccls-cache/@home@runner@Lab-Programs/Lab_5@main.cpp
@@ -8,82 +8,86 @@ using namespace std;
  *
  * @param RPG opponent, RPG *opponent
 **/
-//void player1Turn(RPG player1, RPG player2){
-//  cout << player1.getName() << "'s health: " << player1.getHealth() << "  "<< player2.getName() << "'s health: " << player2.getHealth() << endl;
-//  cout<< player1.getName() << "'s turn" << endl;
-//  cout << "Skill 0: " << "slash" << endl;
-//  cout << "Skill 1: " << "parry" << endl;
-//  cout << "Choose a skill to use, enter 0 or 1: " << endl;
-//  string choice = "";
-//  cin >> choice;
-//  player1.attack(&player2);
-//  if (choice == "0"){
-//    player1.printAction("slash", player2);
-//  } else {
-//    player1.printAction("parry", player2);
-//  }
-//  player1.attack(&player2);
-//  cout << "------------------------" << endl;
-//}
-//void player2Turn(RPG player2, RPG player1){
-//  cout << player1.getName() << "'s health: " << player1.getHealth() //<< "  " << player2.getName() << "'s health: " << player2.getHealth() //<< endl;
-//  cout<< player2.getName() << "'s turn" << endl;
-//  cout << "Skill 0: " << "pilfer" << endl;
-//  cout << "Skill 1: " << "jab" << endl;
-//  cout << "Choose a skill to use, enter 0 or 1: " << endl;
-//  string choice = "";
-//  cin >> choice;
-//  player2.attack(&player1);
-//  if (choice == "0"){
-//    player2.printAction("slash", player1);
-//  } else {
-//    player2.printAction("parry", player1);
-//  }
-//  player2.attack(&player1);
-//  cout << "------------------------" << endl;
-//}
+void player1Turn(RPG player1, RPG player2){
+  cout << player1.getName() << "'s health: " << player1.getHealth() << "  "<< player2.getName() << "'s health: " << player2.getHealth() << endl;
+  cout<< player1.getName() << "'s turn" << endl;
+  cout << "Skill 0: " << "slash" << endl;
+  cout << "Skill 1: " << "parry" << endl;
+  cout << "Choose a skill to use, enter 0 or 1: " << endl;
+  string choice = "";
+  cin >> choice;
+  player1.attack(&player2);
+  if (choice == "0"){
+    player1.printAction("slash", player2);
+  } else {
+    player1.printAction("parry", player2);
+  }
+  player1.attack(&player2);
+  cout << "------------------------" << endl;
+}
+void player2Turn(RPG player2, RPG player1){
+  cout << player1.getName() << "'s health: " << player1.getHealth() << "  " << player2.getName() << "'s health: " << player2.getHealth() << endl;
+  cout<< player2.getName() << "'s turn" << endl;
+  cout << "Skill 0: " << "pilfer" << endl;
+  cout << "Skill 1: " << "jab" << endl;
+  cout << "Choose a skill to use, enter 0 or 1: " << endl;
+  string choice = "";
+  cin >> choice;
+  player2.attack(&player1);
+  if (choice == "0"){
+    player2.printAction("slash", player1);
+  } else {
+    player2.printAction("parry", player1);
+  }
+  player2.attack(&player1);
+  cout << "------------------------" << endl;
+}
 /**
  * @brief uses strength to attack another RPG
  *
  * @param RPG opponent
 **/
-//void RPG::attack(RPG *opponent){
-//  int damage = strength - opponent->getDefense();
-//  int new_health = opponent->getHealth() - damage;
-//  opponent->updateHealth(new_health);
-//}
+void RPG::attack(RPG *opponent){
+  int damage = strength - opponent->getDefense();
+  int new_health = opponent->getHealth() - damage;
+  opponent->updateHealth(new_health);
+}
 /**
  * @brief uses the skills of player1/player2
  *
  * @param RPG
 **/
-//void RPG::useSkill(RPG *opponent){
-//}
+void RPG::useSkill(RPG *opponent){
+}
 
 
 int main(){
   RPG player1 = RPG();
   RPG player2 = RPG("Thief", 100, 10, 5, "thief");
-  
-  cout << player1.getHealth() << endl;
+
+  cout << "Defense: "<< player1.getDefense() << " Strength: " << player1.getStrength() << " Name: " << player1.getName() << endl;
+  cout << "Base Health: " <<player1.getHealth() << endl;
   int new_health = player1.getHealth() - 10;
   player1.updateHealth(new_health);
-  cout << player1.getHealth() << endl;
+  cout << "New Health: "<<player1.getHealth() << endl;
 
-  cout << player2.getHealth() << endl;
+  cout << "Defense: "<< player2.getDefense() << " Strength: " << player2.getStrength() << " Name: " << player2.getName() << endl;
+  cout << "Base Health: " << player2.getHealth() << endl;
   int new_health2 = player2.getHealth() - 10;
   player2.updateHealth(new_health2);
-  cout << player2.getHealth() << endl;
+  cout << "New Health: " << player2.getHealth() << endl;
 
-  cout << player1.isAlive() << endl;
-  cout << player2.isAlive() << endl;
-//  while(player1.isAlive() && player2.isAlive()){
-//    player1Turn(player1, player2);
-//    player2Turn(player2, player1);
-//  }
-//  if (player1.isAlive() == false){
-//    cout << player2.getName() << " defeated " << player1.getName() //<< endl;
-//  } else {
-//    cout << player1.getName() << " defeated " << player2.getName() << endl;
-//  }
+  cout << "1 means the players are alive, 0 means they are dead" << endl;
+  cout << "Player 1: "<< player1.isAlive() << endl;
+  cout << "Player 2: " << player2.isAlive() << endl;
+
+  while(player1.isAlive() && player2.isAlive()){
+    player1Turn(player1, player2);
+    player2Turn(player2, player1);
+  }
+  if (player1.isAlive() == false){
+    cout << player2.getName() << " defeated " << player1.getName() << endl;
+  } else {
+    cout << player1.getName() << " defeated " << player2.getName() << endl;
+  }
 }
