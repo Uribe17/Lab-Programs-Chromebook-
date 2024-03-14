0d0cde3de98ed96bcfb970b2ca64d136e4344864
Uribe17 <157755307+Uribe17@users.noreply.github.com>
2024-03-14 18:17:05 +0000

    update

--- Examples/PointerEX.cpp
+++ Examples/PointerEX.cpp
@@ -14,9 +14,23 @@ void dec(int * y){
 
 int main() {
   int example = 3;
+  int example2 = 5;
+  //Instead of using &, you can use the command "new"
   int *p = &example;
-  dec(p);
-  dec(example);
-  cout << &example << endl;
-  cout << example << endl;
+  int *p2 = &example2; 
+  
+  cout << "Memory address of p: "<< p << endl;
+  cout << "Example variable size (bytes): " << sizeof(example) << endl;
+
+  cout << "Memory address of p2: "<< p2 << endl;
+  cout << "Example2 variable size (bytes): " << sizeof(example2) << endl;
+  
+  //If you don't use the star the output will be the address of example.
+  cout << "Before p Re-assign: " << *p << endl;
+  example = 7;
+  cout << "After p Re-assign: " <<*p << endl;
+
+  cout << "Before p2 Re-assign: " << *p2 << endl;
+  example2 = example * 4;
+  cout << "After p2 Re-assign: " << *p2 << endl;
 }
