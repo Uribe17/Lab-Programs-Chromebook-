a1e074526101b1bd1f48b22001d99fa753c09a35
Uribe17 <157755307+Uribe17@users.noreply.github.com>
2024-03-17 05:14:43 +0000

    update

---
+++ .ccls-cache/@home@runner@Lab-Programs/Lab_5-6@RPG.h
@@ -0,0 +1,39 @@
+#ifndef RPG_H
+#define RPG_H
+#include <string>
+using namespace std;
+
+const int SKILL_SIZE = 2;
+
+class RPG{
+    public:
+        //Constructors
+        RPG();
+        RPG(string name, int health, int strength, int defense, string type);
+
+        void setSkills();
+        void printAction(string, RPG);
+        // Mutator
+        void updateHealth(int);
+
+        //These use pointers, and are not used.
+        void attack(RPG *opponent);
+        void useSkill(RPG *opponent);
+       
+
+        bool isAlive() const;
+        // Accessors
+        string getName() const;
+        int getHealth() const;
+        int getStrength() const;
+        int getDefense() const;
+
+    private:
+        string name;
+        int health;
+        int strength;
+        int defense;
+        string type; //Warrior, Mage, Thief, Archer
+        string skills[SKILL_SIZE];
+};
+#endif
