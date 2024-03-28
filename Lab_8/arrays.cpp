f383c43efa144ef3304218f23cd94775d7dfc35b
Uribe17 <157755307+Uribe17@users.noreply.github.com>
2024-03-28 17:27:53 +0000

    updates

--- Lab_8/arrays.cpp
+++ Lab_8/arrays.cpp
@@ -3,21 +3,22 @@
 using namespace std;
 
 /**
- * @brief
+ * @brief prints the elements in the array & their mem locations
  *
- * @param
- * @param
+ * @param arr - Array of integers
+ * @param size - Size of the array (# of elements)
 **/
 void printMemArr(const int * arr, int size){
   printf("Array - Each int is worth %lu bytes.\n", sizeof(arr[0]);
   for(int i = 0; i < size; i++){
-    
+    printf("Value :%i at Memory Location: %p\n", arr[i], arr +i);
   }
 }
+
 /**
- * @brief
+ * @brief 
  *
- * @param
+ * @param 
  * @param
 **/
 void incArrBy10(){
