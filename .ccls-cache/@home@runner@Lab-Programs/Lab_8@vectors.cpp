c0617b444bfa6318cd2bbc317b23a373a00e7ba7
Uribe17 <157755307+Uribe17@users.noreply.github.com>
2024-04-01 20:09:11 +0000

    updates

---
+++ .ccls-cache/@home@runner@Lab-Programs/Lab_8@vectors.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * @brief prints the elements in the vector & their mem locations
+ *
+ * @param v - Vector of Integers
+**/
+void printMemVec(vector<int> * V, int size){
+  printf("Array - Each int is worth %lu bytes.\n", sizeof(V[0]));
+  for(int i = 0; i < size; i++){
+    printf("Value :%i at Memory Location: %p\n", (*V)[i], V +i);
+  }
+}
+
+/**
+ * @brief Increments all of the elements in the array by 10.
+ *
+ * @param arr
+ * @param size
+**/
+void incVecBy10(vector<int> * V, int size){
+  for(int i = 0; i < size; i++){
+    (*V)[i] += 10;
+  }
+}
+
+int main(){
+  const int SIZE = 5;
+  vector<int> vMain(SIZE);
+
+  for(int i = 0; i < SIZE; i++){
+    vMain.insert(vMain.begin() + i, 100 + i);
+  }
+
+  printf("Before Increment=====================");
+  printMemVec(&vMain, SIZE);
+  incVecBy10(&vMain, SIZE);
+  printf("After Increment=====================");
+  printMemVec(&vMain, SIZE);
+
+  vMain.pop_back();
+  printf("After Pop=====================");
+  printMemVec(&vMain, SIZE);
+  vMain.push_back(101);
+  vMain.push_back(102);
+  printf("After Push=====================");
+  printMemVec(&vMain, SIZE);
+  
+  return 0;
+}
