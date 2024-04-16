58084dbf873772b02b8edf039ce53074c7b7712b
Uribe17 <157755307+Uribe17@users.noreply.github.com>
2024-04-16 16:30:03 +0000

    added new lab

---
+++ Lab_9/search.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ctime>
+using namespace std;
+
+/**
+ * @brief: Returns the first index of an elem if it exists, otherwise returns -1. iterativeSearch starts at the first index and iterates sequentially to the next until it either finds the element or until it reaches to the end.
+ *
+ * @param: Vector of elements
+ * @param: integer to look for
+ * @return: int
+**/
+int iterativeSearch(vector<int> v, int elem){
+  for(int i = 0; i < v.size(); i++){
+    if (v[i] == elem){
+      return i;
+    }
+  }
+  return -1;
+}
+
+/**
+ * 
+ *
+ *
+**/
+
+
+/**
+ * 
+ *
+ *
+**/
+
+
+int main(){
+  
+}
