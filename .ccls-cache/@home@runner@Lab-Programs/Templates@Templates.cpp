738a455e34fc1eed420f5dedfa3d3f92b7a72e3b
Uribe17 <157755307+Uribe17@users.noreply.github.com>
2024-04-23 18:12:33 +0000

    update

--- .ccls-cache/@home@runner@Lab-Programs/Templates@Templates.cpp
+++ .ccls-cache/@home@runner@Lab-Programs/Templates@Templates.cpp
@@ -4,11 +4,18 @@
 using namespace std;
 
 template<typename T>
-int iterativeSearch(vector<int> v, int elem){
+T iterativeSearch(vector<T> v, T elem){
   for(int i = 0; i < v.size(); i++){
     if (v[i] == elem){
       return i;
     }
   }
   return -1;
+}
+
+int main(){
+  vector<int> v1 = {1, 2, 3, 4, 5};
+  vector<double> v2 = {5,10,12.5,14.8,15.1};
+
+  
 }
